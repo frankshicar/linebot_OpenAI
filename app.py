b43594e1b5a2d3e1e2b3f675065000283428192c
frankshicar <93965977+frankshicar@users.noreply.github.com>
2024-01-17 16:37:41 +0800

    text-davinci-003

--- app.py
+++ app.py
@@ -28,12 +28,11 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def GPT_response(text):
     # 接收回應
-    response = openai.Completion.create(model="text-davinci-004", prompt=text, temperature=0.5, max_tokens=500)
+    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
     print(response)
     # 重組回應
     answer = response['choices'][0]['text'].replace('。','')
     return answer
-
 
 #test
 # 監聽所有來自 /callback 的 Post Request
