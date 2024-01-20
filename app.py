ed1535a51ece4788914e126f54c311418c0dcae3
frankshicar <93965977+frankshicar@users.noreply.github.com>
2024-01-20 21:36:13 +0800

    pip install mysql-connector-python

--- app.py
+++ app.py
@@ -87,7 +87,6 @@
 #     port = int(os.environ.get('PORT', 5000))
 #     app.run(host='0.0.0.0', port=port)
 
-
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
