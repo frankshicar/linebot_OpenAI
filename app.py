1635b4d956fa36b700de31a9184aed20613bde8b
frankshicar <93965977+frankshicar@users.noreply.github.com>
2024-01-20 22:33:58 +0800

    db_config change

--- app.py
+++ app.py
@@ -109,11 +109,11 @@ handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
 # 設定 MySQL 連接資訊
 db_config = {
-    'host': '118.167.3.50',
+    'host': 'localhost',
     'port' : 3306,
-    'user': 'root',
-    'password': 'frank0403',
-    'database': 'testdbstupid',
+    'user': 'linebot',
+    'password': '123456789',
+    'database': 'new_schema',
     'charset': 'utf8'
 }
 
